92e853cf5a4a09b7984836ce3667533359c93649
Dave Pearson <davep@davep.org>
2025-01-14 14:30:25 +0000

    :sparkles: Add the very core of the application

---
+++ src/peplum/app/peplum.py
@@ -0,0 +1,13 @@
+"""Provides the main application class."""
+
+##############################################################################
+# Textual imports.
+from textual.app import App
+
+
+##############################################################################
+class Peplum(App[None]):
+    """The main application class."""
+
+
+### peplum.py ends here
