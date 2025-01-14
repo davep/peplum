de5b565d382984f897f3b9e41c3492e2920381e3
Dave Pearson <davep@davep.org>
2025-01-14 13:14:57 +0000

    :card_index: Export API and PEP from the peps module

--- src/peplum/peps/__init__.py
+++ src/peplum/peps/__init__.py
@@ -1,3 +1,12 @@
 """Provides code for downloading and holding PEP information."""
 
+##############################################################################
+# Local imports.
+from .api import API
+from .pep import PEP
+
+##############################################################################
+# Exports.
+__all__ = ["API", "PEP"]
+
 ### __init__.py ends here
