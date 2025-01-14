a6563b80a83d297e19593e31b633b2bcfa5d60e1
Dave Pearson <davep@davep.org>
2025-01-14 13:12:12 +0000

    :sparkles: Add the basic API for downloading PEP data

---
+++ src/peplum/peps/__init__.py
@@ -0,0 +1,3 @@
+"""Provides code for downloading and holding PEP information."""
+
+### __init__.py ends here
---
+++ src/peplum/peps/pep.py
@@ -0,0 +1,189 @@
+"""Provides a class for holding data about a PEP."""
+
+##############################################################################
+# Backward compatibility.
+from __future__ import annotations
+
+##############################################################################
+# Python imports.
+from dataclasses import dataclass
+from datetime import date, datetime
+from locale import LC_TIME, getlocale, setlocale
+from re import Pattern, compile
+from typing import Any, Final, Literal, cast
+
+##############################################################################
+PEPStatus = Literal[
+    "Draft",
+    "Active",
+    "Accepted",
+    "Provisional",
+    "Deferred",
+    "Rejected",
+    "Withdrawn",
+    "Final",
+    "Superseded",
+]
+"""The possible status values for a PEP."""
+
+##############################################################################
+PEPType = Literal[
+    "Standards Track",
+    "Informational",
+    "Process",
+]
+"""The possible types of PEP."""
+
+
+##############################################################################
+def parse_date(date_value: str) -> date:
+    """Parse the sort of date found in the PEP index.
+
+    Args:
+        date_value: The value to parse.
+
+    Returns:
+        A `date` object.
+
+    Notes:
+        The dates in the PEP index use a less-than-ideal date format, being
+        of the form DD-MMM-YYYY where MMM is a truncated form of the month
+        name in English. With this in minds the locale is set in the hope
+        that it'll help with the parsing with strptime.
+    """
+    save_locale = getlocale(LC_TIME)
+    try:
+        setlocale(LC_TIME, "en_US")
+        return datetime.strptime(date_value, "%d-%b-%Y").date()
+    finally:
+        setlocale(LC_TIME, save_locale)
+
+
+##############################################################################
+DATE_ONLY: Final[Pattern[str]] = compile(r"^\d{2}-\w{3}-\d{4}$")
+"""Regular expression for detecting just a date in the input."""
+URL_ONLY: Final[Pattern[str]] = compile("^http")
+"""Regular expression for detecting just an URL in the input."""
+DATE_AND_URL: Final[Pattern[str]] = compile(
+    r"^`(?P<date>\d{2}-\w{3}-\d{4}).+<(?P<url>https:.*)>`__$"
+)
+"""Regular expression for detecting a date and URL in the input."""
+
+
+##############################################################################
+@dataclass(frozen=True)
+class PostHistory:
+    """Details of an item in a PEP's post history."""
+
+    date: date | None = None
+    """The date of the post history."""
+    url: str | None = None
+    """The URL of a link to the post, if there is one."""
+
+    @classmethod
+    def from_value(cls, value: str | None) -> PostHistory | None:
+        """Create a post history object from the given value.
+
+        Args:
+            value: The value to create the history from.
+
+        Returns:
+            A `PostHistory` instance, or `None` if `None` was the input.
+        """
+        if value is None:
+            return value
+        if match := DATE_ONLY.match(value):
+            return PostHistory(date=parse_date(value))
+        if match := URL_ONLY.match(value):
+            return PostHistory(url=value)
+        if match := DATE_AND_URL.match(value):
+            return PostHistory(date=parse_date(match["date"]), url=match["url"])
+        raise ValueError(f"Can't parse `{value}` as PostHistory")
+
+
+##############################################################################
+@dataclass(frozen=True)
+class PEP:
+    """A class that holds data about a PEP."""
+
+    number: int
+    """The numbe of the PEP."""
+    title: str
+    """The title of the PEP."""
+    authors: tuple[str, ...]
+    """The authors of the PEP."""
+    sponsor: str | None
+    """The sponsor of the PEP."""
+    delegate: str | None
+    """The name of the PEP's delegate."""
+    discussions_to: str | None
+    """The location where discussions about this PEP should take place."""
+    status: PEPStatus
+    """The status of the PEP."""
+    type: PEPType
+    """The type of the PEP."""
+    topic: str
+    """The topic of the PEP."""
+    requires: tuple[int, ...]
+    """The PEPS that this PEP requires."""
+    created: date
+    """The date the PEP was created."""
+    python_version: str | None
+    """The Python version this PEP relates to."""
+    post_history: tuple[PostHistory, ...]
+    """The dates of the posting history for the PEP."""
+    resolution: PostHistory | None
+    """The resolution of the PEP, if it has one."""
+    replaces: tuple[int, ...]
+    """The PEPs this PEP replaces."""
+    superseded_by: int | None
+    """The PEP that supersedes this PEP."""
+    url: str
+    """The URL for the PEP."""
+
+    @classmethod
+    def from_json(cls, data: dict[str, Any]) -> PEP:
+        """Create a PEP from the given JSON data.
+
+        Args:
+            data: The data to create the object from.
+
+        Returns:
+            A fresh `PEP` object created from the data.
+        """
+
+        def get_ints(field: str) -> tuple[int, ...]:
+            if isinstance(values := data[field], str):
+                return tuple(int(value) for value in values.split(","))
+            return ()
+
+        return cls(
+            number=data.get("number", -1),
+            title=data.get("title", ""),
+            authors=tuple(
+                author.strip() for author in data.get("authors", "").split(",")
+            ),
+            sponsor=data.get("sponsor"),
+            delegate=data.get("delegate"),
+            discussions_to=data.get("discussions_to"),
+            status=cast(PEPStatus, data.get("status")),
+            type=cast(PEPType, data.get("type")),
+            topic=data.get("topic", ""),
+            requires=get_ints("requires"),
+            created=parse_date(data.get("created", "")),
+            python_version=data.get("python_version"),
+            post_history=tuple(
+                PostHistory.from_value(post.strip()) or PostHistory()
+                for post in (data.get("post_history", "") or "").split(",")
+                if post
+            ),
+            resolution=PostHistory.from_value(data.get("resolution")),
+            replaces=get_ints("replaces"),
+            superseded_by=None
+            if data.get("superseded_by") is None
+            else int(data["superseded_by"]),
+            url=data.get("url", ""),
+        )
+
+
+### pep.py ends here
