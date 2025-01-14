ecad26ab1c24e3005e5bb6c0a0182fef1a1b6c01
Dave Pearson <davep@davep.org>
2025-01-14 16:51:22 +0000

    :sparkles: Add the version number to the header

--- src/peplum/app/screens/main.py
+++ src/peplum/app/screens/main.py
@@ -6,10 +6,16 @@ from textual.app import ComposeResult
 from textual.screen import Screen
 from textual.widgets import Footer, Header
 
+##############################################################################
+# Local imports.
+from ... import __version__
+
 
 ##############################################################################
 class Main(Screen[None]):
     """The main screen for the application."""
+
+    TITLE = f"Peplum v{__version__}"
 
     def compose(self) -> ComposeResult:
         """Compose the content of the main screen."""
