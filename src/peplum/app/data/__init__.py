bd13acb650e6c2128c104260f14d54867f6bbb27
Dave Pearson <davep@davep.org>
2025-01-15 12:57:54 +0000

    :card_index: Export configuration functions

--- src/peplum/app/data/__init__.py
+++ src/peplum/app/data/__init__.py
@@ -1,3 +1,11 @@
 """Provides functions and classes for managing the app's data."""
 
+##############################################################################
+# Local imports.
+from .config import load_configuration, save_configuration, update_configuration
+
+##############################################################################
+# Exports.
+__all__ = ["load_configuration", "save_configuration", "update_configuration"]
+
 ### __init__.py ends here
