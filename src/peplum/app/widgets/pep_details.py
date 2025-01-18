d0a51a5e599da181c427580ede2985ae25f589ad
Dave Pearson <davep@davep.org>
2025-01-18 16:09:04 +0000

    :sparkles: Add a display of a PEP's details to the detail widget
    In retrospect, I don't think I like this approach. I thought it was going to
    be quick and simple to use Markdown and, while it is, and while the result
    looks fine, there are a couple of issues.
    
    The first is that if this is visible it makes navigation through the list
    view really stuttery. The second is that there's a lot of useful information
    in the widget that the user might want to interact with, that won't work
    with the keyboard (because Textual's Markdown widget is keyboard-hostile).
    
    So I think I'm going to roll with this for now while I get on with other
    things, but I think I'll rewrite this soon, perhaps before I finally release
    this app.

--- src/peplum/app/widgets/pep_details.py
+++ src/peplum/app/widgets/pep_details.py
@@ -1,19 +1,134 @@
 """Provides a widget for showing a PEP's details."""
 
 ##############################################################################
+# Python imports.
+from datetime import datetime
+from inspect import cleandoc
+
+##############################################################################
+# Humanize imports
+from humanize import naturaltime
+
+##############################################################################
 # Textual imports.
 from textual.app import ComposeResult
 from textual.containers import VerticalScroll
-from textual.widgets import Label, Rule
+from textual.reactive import var
+from textual.widgets import Markdown
+
+##############################################################################
+# Local imports.
+from ...peps import PEP, PostHistory
 
 
 ##############################################################################
 class PEPDetails(VerticalScroll):
     """A widget for showing details of a PEP."""
 
+    DEFAULT_CSS = """
+    PEPDetails {
+        Markdown {
+            padding: 0 1 0 1;
+            background: transparent;
+            MarkdownH2 {
+                margin: 0;
+            }
+        }
+    }
+    """
+
+    pep: var[PEP | None] = var(None)
+    """The PEP to show the details of."""
+
     def compose(self) -> ComposeResult:
-        yield Rule()
-        yield Label("Something wonderful will happen")
+        yield Markdown()
+
+    @staticmethod
+    def post_as_markdown(post: PostHistory) -> str:
+        """Format a post for Markdown.
+
+        Args:
+            post: The post to format.
+
+        Returns:
+            The formatted post.
+        """
+        if post.date and not post.url:
+            return f"{post.date} ({naturaltime(datetime.combine(post.date, datetime.min.time()))})"
+        if not post.date and post.url:
+            return post.url
+        assert post.date is not None  # It really is not None here.
+        return f"[{post.date} ({naturaltime(datetime.combine(post.date, datetime.min.time()))})]({post.url})"
+
+    @property
+    def pep_post_history_as_markdown(self) -> str:
+        """The PEP's post history as Markdown."""
+        if not self.pep or not self.pep.post_history:
+            return ""
+        return ", ".join(self.post_as_markdown(post) for post in self.pep.post_history)
+
+    @property
+    def pep_as_markdown(self) -> str:
+        """The PEP as Markdown."""
+
+        if self.pep is None:
+            return "No PEP selected"
+
+        return cleandoc(f"""
+        # {self.pep.title}
+
+        ## Author{"" if len(self.pep.authors) == 1 else "s"}
+        {", ".join(self.pep.authors)}
+
+        {"## Sponsor" if self.pep.sponsor else ""}
+        {self.pep.sponsor or ""}
+
+        {"## Delegate" if self.pep.delegate else ""}
+        {self.pep.delegate or ""}
+
+        {"## Discussions to" if self.pep.discussions_to else ""}
+        {self.pep.discussions_to or ""}
+
+        ## Status
+        {self.pep.status}
+
+        ## Type
+        {self.pep.type}
+
+        {"## Topic" if self.pep.topic else ""}
+        {(self.pep.topic or "").capitalize()}
+
+        {"## Requires" if self.pep.requires else ""}
+        {", ".join(str(pep) for pep in self.pep.requires) or ""}
+
+        {"## Replaces" if self.pep.replaces else ""}
+        {", ".join(str(pep) for pep in self.pep.replaces) or ""}
+
+        {"## Superseded By" if self.pep.superseded_by is not None else ""}
+        {"" if self.pep.superseded_by is None else self.pep.superseded_by}
+
+        ## Created
+        {self.pep.created} - {naturaltime(datetime.combine(self.pep.created, datetime.min.time()))}
+
+        {"## Python Version" if self.pep.python_version else ""}
+        {", ".join(self.pep.python_version) or ""}
+
+        {"## Post History" if self.pep.post_history else ""}
+        {self.pep_post_history_as_markdown.strip()}
+
+        {"## Resolution" if self.pep.resolution else ""}
+        {"" if self.pep.resolution is None else self.post_as_markdown(self.pep.resolution)}
+
+        ## URL
+        {self.pep.url}
+        """)
+
+    def watch_pep(self) -> None:
+        """React to the PEP being changed."""
+        self.border_title = (
+            f"PEP {self.pep.number}" if self.pep is not None else "No PEP selected"
+        )
+        self.query_one(Markdown).update(self.pep_as_markdown)
 
 
 ### pep_details.py ends here
