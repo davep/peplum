f66a92d6a0b7cd90ffd36acc963d6184ada840fe
Dave Pearson <davep@davep.org>
2026-02-03 13:42:37 +0000

    :shirt: Update some type hints

--- src/peplum/app/widgets/navigation.py
+++ src/peplum/app/widgets/navigation.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 ##############################################################################
 # Python imports.
-from typing import Callable
+from collections.abc import Callable
 
 ##############################################################################
 # Rich imports.
